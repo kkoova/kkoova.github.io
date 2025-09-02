8efea7b8562e26f20909421dfe01353125a9ae3c
k. ova <127510651+kkoova@users.noreply.github.com>
2025-09-02 22:52:39 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
 function checkAnswer(correctAnswer, nextLevel) {
-      const user = document.getElementById("answer").value.trim();
-      const result = document.getElementById("result");
+  const user = document.getElementById("answer").value.trim();
+  const result = document.getElementById("result");
 
-      if (user === correctAnswer) {
-        result.textContent = `Верно! Флаг: ${correctAnswer}. Переходим к следующему уровню...`;
-        result.style.color = "lime";
+  if (user === correctAnswer) {
+    result.textContent = `Верно! Флаг: ${correctAnswer}. Переходим к следующему уровню...`;
+    result.style.color = "lime";
 
-        // Переход через 2 секунды
-        setTimeout(() => {
-          window.location.href = nextLevel;
-        }, 2000);
-      } else {
-        result.textContent = "Неверно, попробуй снова.";
-        result.style.color = "red";
+    // Переход через 2 секунды
+    setTimeout(() => {
+      if (nextLevel) {
+        window.location.href = nextLevel;
       }
-    }
+    }, 2000);
+  } else {
+    result.textContent = "Неверно, попробуй снова.";
+    result.style.color = "red";
+  }
+}
