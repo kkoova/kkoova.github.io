1da2c9124b4a023c00cb96dd8814b7bb9bdddca7
k. ova <127510651+kkoova@users.noreply.github.com>
2026-04-27 00:29:18 +0300

    Refactor user insertion script for Docker MySQL

--- settLab7.sh
+++ settLab7.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-
-CONTAINER="dvwa"
-DB="dvwa"
-USER="root"
-PASS="password"
-
-docker exec -i $CONTAINER mysql -u $USER -p$PASS $DB <<EOF
-
-INSERT INTO users (username, password) VALUES ('suzy', MD5('good-luck'));
-INSERT INTO users (username, password) VALUES ('Waterloo', MD5('robocop'));
-INSERT INTO users (username, password) VALUES ('nike', MD5('good'));
-INSERT INTO users (username, password) VALUES ('disco', MD5('chameleon'));
-INSERT INTO users (username, password) VALUES ('Golden', MD5('Freddy'));
-
-EOF
-
-echo "5 users added"
+read -p "Введите докер NAMES: " CONTAINER
+sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar) VALUES (6, 'Test1', 'User1', 'suzy', MD5('good-luck'), 'https://kkoova.github.io/assets/5302999548211360138.jpg');"
+sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar) VALUES (7, 'Test2', 'User2', 'Waterloo', MD5('robocop'), 'https://kkoova.github.io/assets/5302999548211360138.jpg');"
+sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar) VALUES (8, 'Test3', 'User3', 'nike', MD5('good'), 'https://kkoova.github.io/assets/5302999548211360138.jpg');"
+sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar) VALUES (9, 'Test4', 'User4', 'disco', MD5('chameleon'), 'https://kkoova.github.io/assets/5302999548211360138.jpg');"
+sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar) VALUES (10, 'Test5', 'User5', 'Golden', MD5('Freddy'), 'https://kkoova.github.io/assets/5302999548211360138.jpg');"
+echo "Done"
