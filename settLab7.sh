ea963a875562a01a3a9f7dbf0b3a5282028abc25
k. ova <127510651+kkoova@users.noreply.github.com>
2026-04-27 10:12:20 +0300

    Update settLab7.sh

--- settLab7.sh
+++ settLab7.sh
@@ -5,4 +5,5 @@ sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvw
 sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar) VALUES (8, 'Test3', 'User3', 'nike', MD5('good'), 'https://kkoova.github.io/assets/5302999548211360138.jpg');"
 sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar) VALUES (9, 'Test4', 'User4', 'disco', MD5('chameleon'), 'https://kkoova.github.io/assets/5302999548211360138.jpg');"
 sudo docker exec -it "$CONTAINER" mysql -u root -p'p@ssword' -e "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar) VALUES (10, 'Test5', 'User5', 'Golden', MD5('Freddy'), 'https://kkoova.github.io/assets/5302999548211360138.jpg');"
-echo "Done"
+echo "Готово. Пользователи для взлома добавлены"
+echo "Удачи :)"
